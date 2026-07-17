04b2a4d08c634c70fc677995fc3e246eec9137c7
憧憬Licoy <licoycn@gmail.com>
2026-07-17 11:15:38 +0800

    chore(release): bump version to 0.0.6

--- website/docs/.vitepress/theme/constants.ts
+++ website/docs/.vitepress/theme/constants.ts
@@ -1,5 +1,5 @@
 /** App marketing version — keep in sync with project.yml MARKETING_VERSION */
-export const APP_VERSION = '0.0.5'
+export const APP_VERSION = '0.0.6'
 
 export const GITHUB_REPO = 'https://github.com/Licoy/StrokeMouse'
 export const GITHUB_RELEASES = `${GITHUB_REPO}/releases`
